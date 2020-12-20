09787bd1a1dcd3e1703c49dbebabf58073c3e3df
JacobHdez <jacohv98@gmail.com>
2020-12-20 15:18:40 -0600

    Added Scene class

---
+++ OpenGL/OpenGL-Sandbox/src/MyCore/Scene/Scene.cpp
@@ -0,0 +1,19 @@
+#include "Scene.h"
+
+SceneNode::SceneNode()
+	: m_Parent{ nullptr }, m_Mesh{ nullptr }, m_Material{ nullptr }, m_Model(glm::mat4(1.0f))
+{
+}
+
+SceneNode::~SceneNode()
+{
+}
+
+Scene::Scene(float fovy, float aspect, float zNear, float zFar)
+	: m_Camera{ fovy, aspect, zNear, zFar }
+{
+}
+
+Scene::~Scene()
+{
+}
